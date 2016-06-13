56ea5b98aeda512e6950278d4505947c5e480592
Tripp Lyons <tripplyons@gmail.com>
2016-06-13 13:47:14 -0700

    Init

---
+++ main.js
@@ -0,0 +1,43 @@
+function getStyle(el, styleProp) {
+    var value, defaultView = (el.ownerDocument || document).defaultView;
+    // W3C standard way:
+    if (defaultView && defaultView.getComputedStyle) {
+        // sanitize property name to css notation
+        // (hypen separated words eg. font-Size)
+        styleProp = styleProp.replace(/([A-Z])/g, "-$1").toLowerCase();
+        return defaultView.getComputedStyle(el, null).getPropertyValue(styleProp);
+    } else if (el.currentStyle) { // IE
+        // sanitize property name to camelCase
+        styleProp = styleProp.replace(/\-(\w)/g, function(str, letter) {
+            return letter.toUpperCase();
+        });
+        value = el.currentStyle[styleProp];
+        // convert other units to pixels on IE
+        if (/^\d+(em|pt|%|ex)?$/i.test(value)) {
+            return (function(value) {
+                var oldLeft = el.style.left,
+                    oldRsLeft = el.runtimeStyle.left;
+                el.runtimeStyle.left = el.currentStyle.left;
+                el.style.left = value || 0;
+                value = el.style.pixelLeft + "px";
+                el.style.left = oldLeft;
+                el.runtimeStyle.left = oldRsLeft;
+                return value;
+            })(value);
+        }
+        return value;
+    }
+}
+
+window.onload = function() {
+    var downtocontent = document.getElementById("header-img");
+    if(downtocontent)
+        downtocontent.addEventListener('click', function(e) {
+            var menubarheight = parseInt(getStyle(document.getElementById('menu'), 'height'));
+            window.scrollTo({
+                left: 0,
+                top: window.innerHeight - menubarheight,
+                behavior: 'smooth'
+            });
+        });
+};
